ad3dd3fb4b1cb83ffdec22cd903f463f8a47e269
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2023-02-20 18:00:29 +0100

    update application.properties with custom configuration

--- src/main/java/de/memozone/spring/crud/test/controller/HelloController.java
+++ src/main/java/de/memozone/spring/crud/test/controller/HelloController.java
@@ -1,5 +1,6 @@
 package de.memozone.spring.crud.test.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/hello")
 public class HelloController {
 
+    @Value("${welcome.message}")
+    private String welcomeMessage;
     @GetMapping
     public String helloSpring(){
-        return "Welcome to Spring Boot World!!!";
+        return welcomeMessage;
     }
 
 
