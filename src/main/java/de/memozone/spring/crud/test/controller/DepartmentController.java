d962be4f0d3305a6cfc88f9c6dd60b7149f32827
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2023-02-20 07:02:41 +0100

    update DeparmentController with simple embedded logger

--- src/main/java/de/memozone/spring/crud/test/controller/DepartmentController.java
+++ src/main/java/de/memozone/spring/crud/test/controller/DepartmentController.java
@@ -2,8 +2,9 @@ package de.memozone.spring.crud.test.controller;
 
 import de.memozone.spring.crud.test.entity.Department;
 import de.memozone.spring.crud.test.service.DepartmentService;
-import de.memozone.spring.crud.test.service.DepartmentServiceImpl;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,42 +17,45 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
+    private final Logger LOGGER= LoggerFactory.getLogger(DepartmentController.class);
 
     @PostMapping
     public Department saveDepartment(@Valid @RequestBody Department department) {
+        LOGGER.info("Inside saveDepartment of DepartmentController");
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping
     public List<Department> getAllDepartments() {
 
+        LOGGER.info("Inside getAllDepartments of DepartmentController");
         return departmentService.getAllDepartments();
     }
 
     @GetMapping("/{id}")
     public Department getDepartmentById(@PathVariable("id") Long departmentId) {
-
+        LOGGER.info("Inside getDepartmentById of DepartmentController");
         return departmentService.getDepartmentById(departmentId);
 
     }
 
     @DeleteMapping("/{id}")
     public String deleteDepartmentById(@PathVariable("id") Long departmentId) {
-
+        LOGGER.info("Inside deleteDepartmentById of DepartmentController");
         departmentService.deleteDepartmentById(departmentId);
         return "Department delete Successfully";
     }
 
     @PutMapping("/{id}")
     public Department updateDepartment(@PathVariable("id") Long departmentId, @RequestBody Department department) {
-
+        LOGGER.info("Inside updateDepartment of DepartmentController");
         return departmentService.updateDepartment(departmentId, department);
 
     }
 
     @GetMapping("/name/{name}")
     public Department getDepartmentByName(@PathVariable("name") String departmentName ){
-
+        LOGGER.info("Inside getDepartmentByName of DepartmentController");
         return departmentService.getDepartmentByName(departmentName);
     }
 
